4056916f58960f805bd5d42ab053de604f996d57
Alex2890 <angelagrouchybuhayko04@gmail.com>
2025-03-06 08:01:39 +0800

    Updated all the files to date

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from "./router";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
-app.use(router); // Use Vue Router
-app.mount("#app"); // Mount the app
+app.use(router);
+app.use(createPinia());
+app.mount('#app');
